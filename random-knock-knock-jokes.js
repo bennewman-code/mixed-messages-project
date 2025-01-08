0eaf3b7d8b8e17d339a32b73999bbeefdc995374
bennewman-code <51948601+bennewman-code@users.noreply.github.com>
2025-01-08 20:24:53 +1100

    Update random-knock-knock-jokes.js
    Jokes and punchline, punchline in an object

--- random-knock-knock-jokes.js
+++ random-knock-knock-jokes.js
@@ -1,1 +1,9 @@
-const jokes = ['Says', 'I am', 'Art', 'Weekend', 'I.O.']
+const jokes = ['Says', 'I am', 'Art', 'Weekend', 'I.O.'];
+
+const punchline = {
+    Says: "Says me, that's who!",
+    Iam: "I am who is knocking. Who are you?",
+    Art: "R2D2 is my favourite droid in Star Wars!",
+    Weekend: "Weekend do anything we want!",
+    IO: "Me. When are you paying me back?"
+};
