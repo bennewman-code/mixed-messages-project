701e320b8d1fd36594592943d67c5dcbb9fab0a7
bennewman-code <51948601+bennewman-code@users.noreply.github.com>
2025-01-08 20:33:40 +1100

    Update random-knock-knock-jokes.js
    Merged  jokes and random jokes into one function

--- random-knock-knock-jokes.js
+++ random-knock-knock-jokes.js
@@ -1,4 +1,7 @@
-const jokes = ['Says', 'I am', 'Art', 'Weekend', 'I.O.'];
+const randjokes = () => {
+    const jokes = ['Says', 'I am', 'Art', 'Weekend', 'I.O.'];
+    return jokes[Math.floor(Math.random() * 5)];
+}
 
 const punchline = {
     Says: "Says me, that's who!",
@@ -7,3 +10,4 @@ const punchline = {
     Weekend: "Weekend do anything we want!",
     IO: "Me. When are you paying me back?"
 };
+
