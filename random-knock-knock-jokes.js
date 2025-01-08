fbd2a88259a9d0437f3ef327e217f1533f9efb7d
bennewman-code <51948601+bennewman-code@users.noreply.github.com>
2025-01-08 21:54:12 +1100

    Update random-knock-knock-jokes.js
    Using nested objects to store the data and randomly picking one the keys which then bypass the need for else if statements

--- random-knock-knock-jokes.js
+++ random-knock-knock-jokes.js
@@ -1,30 +1,29 @@
-const randjokes = () => {
-    const jokes = ['Says', 'I am', 'Art', 'Weekend', 'I.O.'];
-    return jokes[Math.floor(Math.random() * 5)];
-};
-
-const punchline = {
-    Says: "Says me, that's who!",
-    Iam: "I am who is knocking. Who are you?",
-    Art: "R2D2 is my favourite droid in Star Wars!",
-    Weekend: "Weekend do anything we want!",
-    IO: "Me. When are you paying me back?"
-};
-
-const joke = () => {
-    randjokes()
-    const jk = randjokes()
-    if (jk === 'Says') {
-        console.log(`Knock knock. Who's there? ${jk}. ${jk} Who? ${punchline.Says}`);
-    } else if (jk === 'I am') {
-        console.log(`Knock knock. Who's there? ${jk}. ${jk} Who? ${punchline.Iam}`);
-    } else if (jk === 'Art') {
-        console.log(`Knock knock. Who's there? ${jk}. ${jk} Who? ${punchline.Art}`);
-    } else if (jk === 'Weekend') {
-        console.log(`Knock knock. Who's there? ${jk}. ${jk} Who? ${punchline.Weekend}`);
-    } else if (jk === 'I.O.') {
-        console.log(`Knock knock. Who's there? ${jk} ${jk} Who? ${punchline.IO}`);
+const jokes = {
+    Says: {
+        joke: 'Says',
+        punchline: "Says me, that's who!"
+    },
+    Iam: {
+        joke: 'I am',
+        punchline: "I am who is knocking. Who are you?"
+    },
+    Art: {
+        joke: 'Art',
+        punchline: "R2D2 is my favourite droid in Star Wars!"
+    },
+    Weekend: {
+        joke: 'Weekend',
+        punchline: "Weekend do anything we want!"
+    },
+    IO: {
+        joke: 'I.O.',
+        punchline: "Me. When are you paying me back?"
     }
 };
 
+const joke = () => {
+    const arrkeys = Object.keys(jokes);
+    const jk = arrkeys[Math.floor(Math.random() * arrkeys.length)];
+    console.log(`Knock knock. Who's there? ${jokes[jk].joke}. ${jokes[jk].joke} Who? ${jokes[jk].punchline}`);
+}
 joke();
